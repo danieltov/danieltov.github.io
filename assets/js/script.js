bd92b7949de0370df3f8d81740c228568c40cd70
Daniel Tovar <daniel@danieltovar.io>
2019-06-05 10:17:27 -0600

    Updated message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip();
   ScrollReveal().reveal('.card', { delay: 350 });
   ScrollReveal().reveal('.lazy1', { delay: 350 });
   ScrollReveal().reveal('.lazy2', { delay: 450 });
   ScrollReveal().reveal('.lazy3', { delay: 550 });
   ScrollReveal().reveal('.lazy4', { delay: 650 });
   var typed = new Typed('#typed', {
-    strings: ["Hey.", "I'm Daniel.", "I am a front-end web developer.", "I am a full-stack web developer.", "I am a student.", "I am available for new projects."],
-    smartBackspace: true, // Default value
-    typeSpeed: 35,
+    strings: ["Hey.", "I'm Daniel.", "I am a web developer at Utah Valley University.", "I build cool things with HTML5.", "I build cool things with CSS3.", "I build cool things with JavaScript.", "I build cool things with React.", "I build cool things with Node.", "I build cool things with MongoDB.", "I build cool things with Sequelize.", "Talk to me about working on a project together."],
+    smartBackspace: true,
+    typeSpeed: 45,
     loop: true
   });
 });
