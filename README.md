ee3fc4c703efd295c3dfa1a95ad543480a4e8b86
Daniel Tovar <daniel@danieltovar.io>
2019-06-04 21:58:29 -0600

    added typedJS

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dt-portfolio
+# Daniel Tovar's UUBC Portfolio
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-$(function() {
+$(function () {
   $('[data-toggle="tooltip"]').tooltip();
+  ScrollReveal().reveal('.card', { delay: 350 });
+  ScrollReveal().reveal('.lazy1', { delay: 350 });
+  ScrollReveal().reveal('.lazy2', { delay: 450 });
+  ScrollReveal().reveal('.lazy3', { delay: 550 });
+  ScrollReveal().reveal('.lazy4', { delay: 650 });
+  var typed = new Typed('#typed', {
+    strings: ["Hey.", "I'm Daniel.", "I am a front-end web developer.", "I am a full-stack web developer.", "I am a student.", "I am available for new projects."],
+    smartBackspace: true, // Default value
+    typeSpeed: 35,
+    loop: true
+  });
 });
